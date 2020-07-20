f0c7589061ca7251475b95383655f4e1bef6db79
troy <1553765526@@qq.com>
2020-07-20 13:08:07 +0800

    update config

--- server/core/config.go
+++ server/core/config.go
@@ -11,11 +11,11 @@ import (
 func init() {
 	global.GVA_CONFIG = config.Server{}
 	var allConfigs = make(map[string]interface{})
-	allConfigs["/superconf/union/mysql/admin"] = &global.GVA_CONFIG.Mysql
-	allConfigs["/superconf/union/mysql/biz"] = &global.GVA_CONFIG.BizMysql
+	allConfigs["/superconf/admin/mysql/admin_db"] = &global.GVA_CONFIG.Mysql
+	allConfigs["/superconf/admin/mysql/biz_db"] = &global.GVA_CONFIG.BizMysql
 	allConfigs["/superconf/third_party/qiniu"] = &global.GVA_CONFIG.Qiniu
 	allConfigs["/superconf/admin/casbin"] = &global.GVA_CONFIG.Casbin
-	allConfigs["/superconf/admin/redis"] = &global.GVA_CONFIG.Redis
+	allConfigs["/superconf/admin/redis/admin_redis"] = &global.GVA_CONFIG.Redis
 	allConfigs["/superconf/admin/system"] = &global.GVA_CONFIG.System
 	allConfigs["/superconf/admin/jwt"] = &global.GVA_CONFIG.JWT
 	allConfigs["/superconf/admin/captcha"] = &global.GVA_CONFIG.Captcha
